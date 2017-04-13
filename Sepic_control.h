4cdabd4fbead88eff677dc1b40fc10b680a8b1aa
kaiharten <kaiharten@gmail.com>
2017-04-13 20:21:59 +0200

    Update Sepic_control.h

--- Sepic_control.h
+++ Sepic_control.h
@@ -11,11 +11,10 @@
 
 #include "avr/io.h"
 
-#define milliVolt_2_Bit(val, reso, ref, ampl) (uint16_t)((val*reso)/(ref*ampl))
-#define Bit_2_MilliVolt(val, reso, ref, ampl) (uint16_t)((val*ref*ampl)/reso)
+#define milliVolt_2_Bit(val, reso, ref, ampl)     (uint16_t)((val*reso)/(ref*ampl))
+#define Bit_2_MilliVolt(val, reso, ref, ampl)     (uint16_t)((val*ref*ampl)/reso)
 
-uint8_t Get_FeedForward_Duty(uint16_t src_volt);
-int8_t Get_FeedBack_Correction(uint16_t feedback);
-uint8_t Get_Offset(uint16_t source_adc);
+uint8_t Set_Duty(uint16_t src_volt);
+int8_t Set_Correction(uint16_t feedback);
 
 #endif /* SEPIC_CONTROL_H_ */
