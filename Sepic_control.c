a9a961c668ba8d8a2b99f9a1411c429897a9a437
kaiharten <kaiharten@gmail.com>
2017-04-13 20:21:10 +0200

    Update Sepic_control.c

--- Sepic_control.c
+++ Sepic_control.c
@@ -5,88 +5,20 @@
  *  Author: Kai Harten
  */ 
 #include "Sepic_control.h"
-
-// 118 bij 15V Source
-// 132 bij 12.5 Source
-// 147 bij 10.33V Source
-// 149 bij 10V Source
-		
-uint8_t Get_FeedForward_Duty(uint16_t adc_val)
+	
+uint8_t Set_Duty(uint16_t adc_val)
 {	
 	duty = 171776/(adc_val+671);
 	return duty;
 }
 
-int8_t Get_FeedBack_Correction(uint16_t feedback)
+int8_t Set_Correction(uint16_t feedback)
 {
 	static int8_t   correction	= 0;
 	uint16_t setpoint		= 645;
-	int8_t   error			= setpoint - feedback;
+	int16_t   error			= setpoint - feedback;
+	uint8_t p = 2;
 	
-	if (error >= 45)
-	{
-		correction = 6;
-	}
-	if (error >= 30 && error < 45)
-	{
-		correction = 4;
-	}
-	if (error >= 15 && error < 30)
-	{
-		correction = 2;
-	}
-	if (error < 15 && error > -15)
-	{
-		correction = 0;
-	}
-	if (error <= -15 && error < -30)
-	{
-		correction = -2;
-	}
-	if (error <= -30 && error > -45)
-	{
-		correction = -4;
-	}
-	if (error <= -45)
-	{
-		correction = -6;
-	}
-	if (error <= -65)
-	{
-		correction -= 6;
-	}
-	else
-	{
-		correction = 2;
-	}
+	correction = error*p;
 	return correction;
-}
-
-uint8_t Get_Offset(uint16_t source_adc)
-{
-	static uint8_t offset = 0;
-	
-	if (source_adc <= 640 && source_adc > 590)
-	{
-		offset = 4;
-	}
-	if (source_adc <= 590 && source_adc > 565)
-	{
-		offset = 6;
-	}
-			
-	if (source_adc <= 565 && source_adc > 530)
-	{
-		offset = 7;
-	}
-	if (source_adc <= 530 && source_adc > 510)
-	{
-		offset = 9;
-	}
-	if (source_adc > 715)
-	{
-		offset = 1;
-	}
-			
-	return offset;
 }
